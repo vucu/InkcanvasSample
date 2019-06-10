bf61948dbcbfb3782e225a6d403f7cbef70a8876
Ameed <ameed@gmail.com>
2019-06-10 16:42:23 -0700

    add view model base

---
+++ InkcanvasSample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace InkcanvasSample.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
